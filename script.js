2d97cc350e64384831179082ee662d3663a248f3
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-04-22 14:02:08 -0400

    Adding answers

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// PART 1
+let stringNames = ["Jose", "Darlene", "David", "Jazlyn", "Chai"];
+
+let ages = [23, 24, 22, 16, 1];
+let ageDifference = [22 < 24, 24 > 22, 22 > 16, 16 > 1, 1 < 16];
+
+console.log(stringNames[2], ages[2], ageDifference[2]);
+
+//PART 2
+
+stringNames.push("Daisy", "Pepper", "Millie");
+
+console.log(stringNames, ages, ageDifference);
+
+
